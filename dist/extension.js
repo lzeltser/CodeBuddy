902302372bc5009156814a3d3ce4680cf821985f
ThienTran Le <ttble2001@gmail.com>
2024-03-19 17:33:42 -0400

    got background image working

--- dist/extension.js
+++ dist/extension.js
@@ -31,26 +31,31 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(__webpack_require__(1));
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const customSidebarViewProvider_1 = __webpack_require__(2);
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "code-buddy" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('code-buddy.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
+    // Console diagnostic information (console.log) and errors (console.error)
+    // Will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vscode-extension-sidebar-html" is active!');
+    const provider = new customSidebarViewProvider_1.CustomSidebarViewProvider(context.extensionUri);
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(customSidebarViewProvider_1.CustomSidebarViewProvider.viewType, provider));
+    context.subscriptions.push(vscode.commands.registerCommand("vscodeSidebar.menu.view", () => {
+        const message = "Menu/Title of extension is clicked !";
+        vscode.window.showInformationMessage(message);
+    }));
+    // Command has been defined in the package.json file
+    // Provide the implementation of the command with registerCommand
+    // CommandId parameter must match the command field in package.json
+    let openWebView = vscode.commands.registerCommand('vscodeSidebar.openview', () => {
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from CodeBuddy!');
+        vscode.window.showInformationMessage('Command " Sidebar View [vscodeSidebar.openview] " called.');
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(openWebView);
 }
 exports.activate = activate;
-// This method is called when your extension is deactivated
+// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 
@@ -60,6 +65,92 @@ exports.deactivate = deactivate;
 /***/ ((module) => {
 
 module.exports = require("vscode");
+
+/***/ }),
+/* 2 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CustomSidebarViewProvider = void 0;
+const vscode = __importStar(__webpack_require__(1));
+class CustomSidebarViewProvider {
+    _extensionUri;
+    static viewType = "vscodeSidebar.openview";
+    _view;
+    constructor(_extensionUri) {
+        this._extensionUri = _extensionUri;
+    }
+    resolveWebviewView(webviewView, context, token) {
+        this._view = webviewView;
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
+            localResourceRoots: [this._extensionUri],
+        };
+        webviewView.webview.html = this.getHtmlContent(webviewView.webview);
+    }
+    getHtmlContent(webview) {
+        // Get the local path to main script run in the webview,
+        // then convert it to a uri we can use in the webview.
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets", "main.js"));
+        const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets", "reset.css"));
+        const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets", "vscode.css"));
+        // Same for stylesheet
+        const stylesheetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets", "main.css"));
+        const background = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "assets", "background.png"));
+        // Use a nonce to only allow a specific script to be run.
+        const nonce = getNonce();
+        return `<!DOCTYPE html>
+			<html lang="en">
+
+			<head>
+				<meta charset="UTF-8">
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+        <link href="${stylesheetUri}" rel="stylesheet">
+			</head>
+
+			<body>
+        <img src="${background}" alt="bg here">
+      </body>
+
+			</html>`;
+    }
+}
+exports.CustomSidebarViewProvider = CustomSidebarViewProvider;
+function getNonce() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
 
 /***/ })
 /******/ 	]);
---
+++ src/customSidebarViewProvider.ts
@@ -0,0 +1,77 @@
+import * as vscode from "vscode";
+
+export class CustomSidebarViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "vscodeSidebar.openview";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext<unknown>,
+    token: vscode.CancellationToken
+  ): void | Thenable<void> {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+    webviewView.webview.html = this.getHtmlContent(webviewView.webview);
+  }
+
+  private getHtmlContent(webview: vscode.Webview): string {
+    // Get the local path to main script run in the webview,
+    // then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "main.js")
+    );
+
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "vscode.css")
+    );
+
+    // Same for stylesheet
+    const stylesheetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "main.css")
+    );
+
+    const background = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "assets", "background.png")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+
+			<head>
+				<meta charset="UTF-8">
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+        <link href="${stylesheetUri}" rel="stylesheet">
+			</head>
+
+			<body>
+        <img src="${background}" alt="bg here">
+      </body>
+
+			</html>`;
+  }
+}
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
